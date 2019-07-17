e1c5550b8a6fb4cd8d8fd191f602517d25668431
Ramon Chavez <rachavez1974@gmail.com>
2019-07-17 14:55:11 -0400

    Add index username and uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190717174829) do
+ActiveRecord::Schema.define(version: 20190717183544) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
