00d407400cc929fb7158d4fbf2cc1b56ccf8ac94
Ramon Chavez <rachavez1974@gmail.com>
2019-07-17 15:41:14 -0400

    Add #make_deposit to and fix '/account'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :username, :password
+
+  def make_deposit(amount)
+    user_balance = self.balance
+    new_balance = user_balance + amount
+    self.update_attribute(:balance, new_balance)
+  end
+
+  def withdraw_money
+    
+  end
 end
