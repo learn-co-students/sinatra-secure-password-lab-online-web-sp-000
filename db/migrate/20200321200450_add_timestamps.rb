e2b63fae85266eac5a582fc5d2b9a188038fd700
ottocedeno <otto@creativespaceclub.com>
2020-03-21 16:06:57 -0400

    added balance

---
+++ db/migrate/20200321200450_add_timestamps.rb
@@ -0,0 +1,5 @@
+class AddTimestamps < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :timestamps, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200321185405) do
+ActiveRecord::Schema.define(version: 20200321194128) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.integer "balance"
   end
 
 end
