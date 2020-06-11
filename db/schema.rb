33f5eaf6bdaf79429e23d841bdfeee507b633211
cantstopcoding <jonathan_koss@yahoo.com>
2020-06-11 18:47:26 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20200611023917) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
