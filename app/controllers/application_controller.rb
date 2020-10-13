e866a98d331ac03aba5dfb273a1bce87d93e0ad6
kkirby16 <kkirby16@my.whitworth.edu>
2020-10-13 13:56:34 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 require "./config/environment"
 require "./app/models/user"
-class ApplicationController < Sinatra::Base
 
+class ApplicationController < Sinatra::Base
   configure do
     set :views, "app/views"
     enable :sessions
@@ -17,22 +17,31 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    user = User.new(username: params[:username], password: params[:password])
+    if user.save
+      redirect "/login"
+    elsif user.errors.any?
+      redirect "/failure"
+    end
   end
 
-  get '/account' do
+  get "/account" do
     @user = User.find(session[:user_id])
     erb :account
   end
-
 
   get "/login" do
     erb :login
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
   end
 
   get "/failure" do
@@ -53,5 +62,4 @@ class ApplicationController < Sinatra::Base
       User.find(session[:user_id])
     end
   end
-
 end
