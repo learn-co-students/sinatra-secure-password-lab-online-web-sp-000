72ba1808890a565ef0401044017d0df26842e172
javonesm89 <javosmith12@gmail.com>
2020-10-15 14:32:38 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    user = User.new(username: params[:username], password: params[:password])
+    if params[:username] != '' && params[:password] != ''
+      user.save && session[:user_id] = user.id 
+      # --> not needed to pass lab, but use in app build
+      redirect to '/login'
+    else
+      redirect to '/failure'
+    end
   end
 
   get '/account' do
@@ -32,7 +38,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/account'
+    else
+      redirect to '/failure'
+    end
   end
 
   get "/failure" do
