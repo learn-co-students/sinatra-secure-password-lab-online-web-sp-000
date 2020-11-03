319e90a2445153dca93eb8f951bd537851775d1e
concow <ccowan003@comcast.net>
2020-11-03 13:45:21 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
+    if params[:username] == "" || params[:password] == ""   # if username and password are empty
+      redirect '/failure'
+    else
+      User.create(username: params[:username], password: params[:password])
+      redirect '/login'
+    end
 
-  end
+   end
 
   get '/account' do
     @user = User.find(session[:user_id])
