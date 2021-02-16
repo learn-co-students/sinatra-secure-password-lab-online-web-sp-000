7958f188035a663895d9150667370c9a9fc4457d
Christerle Nounoute <christerlesn@gmail.com>
2021-02-16 05:37:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
-    if user.save && :username !=nil
+    if user.save && !user.username.empty?
       redirect "/login"
     else
       redirect "/failure"
