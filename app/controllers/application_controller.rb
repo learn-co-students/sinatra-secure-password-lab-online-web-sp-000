f960e85d578bca6df6521873c268c44926299798
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-02-18 11:19:31 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,9 +18,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    @user = User.new(:username => params[:username], :password => params[:password])
-  # binding.pry
-    if @user.save 
+    user = User.new(:username => params[:username], :password => params[:password])
+    #binding.pry
+    if user.save 
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
+  end
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+    binding.pry
+    if user && user.authenticate(params[:password])
+      #binding.pry
+      session[:user_id] = user.id 
       redirect "/account"
     else
       redirect "/failure"
@@ -28,22 +44,10 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
-  end
-
-
-  get "/login" do
-    erb :login
-  end
-
-  post "/login" do
-    @user = User.find_by(:username => params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id 
-      redirect "/account"
-    else
-      redirect "/failure"
+    if current_user && logged_in?
+      erb :account
+    else  
+      redirect "/login"
     end
   end
 
